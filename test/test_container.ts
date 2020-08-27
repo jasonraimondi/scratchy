a4f6f0939868beb7f008d73f5506cb152b20c783
Jason Raimondi <jason@raimondi.us>
2020-08-26 23:42:16 -0700

    tests: use ethereal email for smtp
    I need to fix this so it isnt making a connection for every test...

--- test/test_container.ts
+++ test/test_container.ts
@@ -2,15 +2,16 @@ import { createConnection } from "typeorm";
 import { v4 } from "uuid";
 import { Test } from "@nestjs/testing";
 import { ModuleMetadata } from "@nestjs/common/interfaces/modules/module-metadata.interface";
+import nodemailer from "nodemailer";
 
 import { databaseProviders } from "~/lib/repositories/repository.providers";
 import { MailerModule } from "@nestjs-modules/mailer";
-import { ENV } from "~/lib/config/environment";
 import { BullModule } from "@nestjs/bull";
 import { QUEUE } from "~/lib/config/keys";
 import { EmailService } from "~/lib/emails/services/email.service";
 
 export async function createTestingModule(metadata: ModuleMetadata, entities: any[] = [], logging = false) {
+  const account = await nodemailer.createTestAccount()
   const repositoryProviders = [
     {
       provide: "DATABASE_CONNECTION",
@@ -26,13 +27,23 @@ export async function createTestingModule(metadata: ModuleMetadata, entities: an
     },
     ...databaseProviders,
   ];
-
+  const auth = {
+    user: account.user, // generated ethereal user
+    pass: account.pass  // generated ethereal password
+  };
+  console.log(auth);
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth
+  });
   const emailImports = [
     BullModule.registerQueue({
       name: QUEUE.email,
     }),
     MailerModule.forRoot({
-      transport: ENV.mailerURL,
+      transport: transporter,
       defaults: {
         from: `"graphql-scratchy" <jason+scratchy@raimondi.us>`,
       },
