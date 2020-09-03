5c43adc6575c8cd42958114dc2128c60a92d0ac6
Jason Raimondi <jason@raimondi.us>
2020-09-02 23:17:25 -0700

    test: add auth_controller specs
    two are marked todo

--- src/app/auth/auth.controller.e2e-spec.ts
+++ src/app/auth/auth.controller.e2e-spec.ts
@@ -10,14 +10,18 @@ import { AuthService } from "~/app/auth/auth.service";
 import { Permission } from "~/entity/role/permission_entity";
 import { AuthController } from "~/app/auth/auth.controller";
 import { createTestingModule } from "~test/app_testing.module";
+import { IUserRepository } from "~/lib/repositories/user/user.repository";
+import { REPOSITORY } from "~/lib/config/keys";
 
 const entities = [EmailConfirmationToken, User, Role, Permission, ForgotPasswordToken];
 
 describe("Auth Controller", () => {
   let app: INestApplication;
   let module: TestingModule;
+  let authService: AuthService;
+  let userRepository: IUserRepository;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     module = await createTestingModule(
       {
         controllers: [AuthController],
@@ -25,7 +29,8 @@ describe("Auth Controller", () => {
       },
       entities,
     );
-
+    authService = module.get(AuthService);
+    userRepository = module.get(REPOSITORY.UserRepository);
     app = module.createNestApplication();
     await app.init();
   });
@@ -34,11 +39,72 @@ describe("Auth Controller", () => {
     await app.close();
   });
 
-  describe("/POST auth/refresh_token", () => {
-    it("throws 401 if no refresh token cookie", () => {
-      return request(app.getHttpServer()).post("/auth/refresh_token").expect(401);
-    });
+  test.skip("refresh token updates successfully", async () => {
+    // arrange
+    const user = await User.create({ email: "jason@raimondi.us" });
+    await userRepository.save(user);
+    const refreshToken = authService.createRefreshToken(user);
 
-    // @todo add test for valid post
+    // act
+    const response = await request(app.getHttpServer())
+      .post("/auth/refresh_token")
+      .expect(200)
+      .set("Cookie", [`jid=${refreshToken}`, `rememberMe=true`])
+      .expect("Content-Type", /json/);
+
+    // assert
+    expect(response.header["set-cookie"].length).toBe(2);
+    expect(response.header["set-cookie"][0]).toMatch(/rememberMe=true*/);
+    const REFRESH_COOKIE = /jid=[a-zA-Z\d\-_]+.[a-zA-Z\d\-_]+.[a-zA-Z\d\-_]+; Domain=localhost; Path=\/;/;
+    expect(response.header["set-cookie"][1]).toMatch(REFRESH_COOKIE);
+    expect(response.body.success).toBe(true);
+    expect(response.body.accessToken).toMatch(/[a-zA-Z\d]+.[a-zA-Z\d]+.[a-zA-Z\d]+/);
+  });
+
+  test.skip("refresh token no remember me", async () => {
+    // arrange
+    const user = await User.create({ email: "jason@raimondi.us" });
+    await userRepository.save(user);
+    const refreshToken = authService.createRefreshToken(user);
+
+    // act
+    const response = await request(app.getHttpServer())
+      .post("/auth/refresh_token")
+      .expect(200)
+      .set("Cookie", [`jid=${refreshToken}`])
+      .expect("Content-Type", /json/);
+
+    // assert
+    expect(response.header["set-cookie"][0]).toMatch(/rememberMe=false*/);
+  });
+
+  test("missing token (jid) fails", async () => {
+    // act
+    const response = await request(app.getHttpServer())
+      .post("/auth/refresh_token")
+      .expect(401)
+      .expect("Content-Type", /json/);
+
+    // assert
+    expect(response.header["set-cookie"]).toBeUndefined();
+    expect(response.body.success).toBe(false);
+    expect(response.body.accessToken).toBe("");
+  });
+
+  test("invalid token (jid) fails", async () => {
+    // arrange
+    const invalidToken = "invalid-token";
+
+    // act
+    const response = await request(app.getHttpServer())
+      .post("/auth/refresh_token")
+      .expect(401)
+      .set("Cookie", [`jid=${invalidToken}`])
+      .expect("Content-Type", /json/);
+
+    // assert
+    expect(response.header["set-cookie"]).toBeUndefined();
+    expect(response.body.success).toBe(false);
+    expect(response.body.accessToken).toBe("");
   });
 });
