2f4d031b3d974d1a57a4e2c0c08d109e2e42f79b
Jason Raimondi <jason@raimondi.us>
2020-09-02 17:06:26 -0700

    feat: add helmet/rate-limit to harden on production

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,26 @@ import "dotenv/config";
 
 import { NestFactory } from "@nestjs/core";
 import { UI as bullUI } from "bull-board";
+import type { NestExpressApplication } from "@nestjs/platform-express";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 import { ENV } from "~/lib/config/environment";
 import { AppModule } from "~/app/app.module";
 
 (async () => {
   if (ENV.enableDebugging) console.log("DEBUGGING ENABLED");
-  const app = await NestFactory.create(AppModule);
-  app.use("/admin/queues", bullUI)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use("/admin/queues", bullUI);
+  if (process.env.NODE_ENV === "production") {
+    app.use(helmet());
+    app.use(
+      rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+      }),
+    );
+  }
   await app.listen(3000);
   console.log(`Listening on ${await app.getUrl()}`);
 })();
