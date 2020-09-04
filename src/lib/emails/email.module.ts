15b67c310a1b591a234be51757de22b6bc146f87
Jason Raimondi <jason@raimondi.us>
2020-09-03 20:41:40 -0700

    refactor: move queue setup and ui into module

--- src/lib/emails/email.module.ts
+++ src/lib/emails/email.module.ts
@@ -1,10 +1,8 @@
 import { MailerModule } from "@nestjs-modules/mailer";
-import { BullModule } from "@nestjs/bull";
 import { Module } from "@nestjs/common";
 
-import { QueueUIProvider } from "~/app/queue/queue_ui.provider";
+import { QueueModule } from "~/lib/queue/queue.module";
 import { ENV } from "~/lib/config/environment";
-import { QUEUE } from "~/lib/config/keys";
 import { emailProviders } from "~/lib/emails/email.providers";
 import { SendEmailProcessor } from "~/lib/emails/processors/send_email.processor";
 import { EmailService } from "~/lib/emails/services/email.service";
@@ -13,6 +11,7 @@ import { RepositoryModule } from "~/lib/repositories/repository.module";
 
 @Module({
   imports: [
+    QueueModule,
     RepositoryModule,
     MailerModule.forRoot({
       transport: ENV.mailerURL,
@@ -20,12 +19,8 @@ import { RepositoryModule } from "~/lib/repositories/repository.module";
         from: `"graphql-scratchy" <jason+scratchy@raimondi.us>`,
       },
     }),
-    BullModule.registerQueue({
-      name: QUEUE.email,
-      redis: ENV.queueURL,
-    }),
   ],
-  providers: [EmailService, EmailTemplateService, SendEmailProcessor, QueueUIProvider, ...emailProviders],
+  providers: [EmailService, EmailTemplateService, SendEmailProcessor, ...emailProviders],
   exports: [...emailProviders],
 })
 export class EmailModule {}
---
+++ src/lib/queue/queue.module.ts
@@ -0,0 +1,20 @@
+import { BullModule } from "@nestjs/bull";
+import { Module } from "@nestjs/common";
+
+import { QueueUIProvider } from "~/lib/queue/queue_ui.provider";
+import { ENV } from "~/lib/config/environment";
+import { QUEUE } from "~/lib/config/keys";
+
+const queue = [
+  BullModule.registerQueue({
+    name: QUEUE.email,
+    redis: ENV.queueURL,
+  }),
+]
+
+@Module({
+  providers: [QueueUIProvider],
+  imports: [...queue],
+  exports: [...queue],
+})
+export class QueueModule {}
