f8998d5836432192e27a62a04bd5a3b0462b5bda
Jason Raimondi <jason@raimondi.us>
2020-09-03 22:20:11 -0700

    test: add user repository paginator test

--- src/lib/repositories/user/user.repository.spec.ts
+++ src/lib/repositories/user/user.repository.spec.ts
@@ -10,7 +10,7 @@ import { IUserRepository } from "~/lib/repositories/user/user.repository";
 import { createTestingModule } from "~test/app_testing.module";
 import { userGenerator } from "~test/generators/user.generator";
 
-describe.skip("user_repository", () => {
+describe("user_repository", () => {
   const entities = [User, Role, Permission, ForgotPasswordToken, EmailConfirmationToken];
 
   let container: TestingModule;
@@ -21,24 +21,26 @@ describe.skip("user_repository", () => {
     container = await createTestingModule({}, entities);
     userRepository = container.get<IUserRepository>(REPOSITORY.UserRepository);
     users = [
-      await userGenerator(),
-      await userGenerator(),
-      await userGenerator(),
-      await userGenerator(),
-      await userGenerator(),
+      await userGenerator({ id: "c3c5df06-b4e7-498b-bdbf-3f3138404b31"}),
+      await userGenerator({ id: "b4e46399-5401-4a58-b7d7-3a8b37bda8a0"}),
+      await userGenerator({ id: "9d589af7-234b-4fbc-b6fa-8a1343b5736e"}),
+      await userGenerator({ id: "02a5887d-9ae5-49b1-8c05-88714b506cab"}),
+      await userGenerator({ id: "55a5db1b-d585-446d-b063-71574edafa9a"}),
     ];
     for (const user of users) {
       await userRepository.save(user);
     }
   });
 
-  test("limits are passed correctly", async () => {
-    const { data, cursor } = await userRepository.list({ limit: 2 });
-    console.log(users.map((user) => user.id));
+  test("limits and order desc", async () => {
+    const { data, cursor } = await userRepository.list({ limit: 3, order: "DESC" });
+    const [user1, user2, user3] = users;
 
-    expect(data[0].id).toBe("users[2].id");
+    expect(data.length).toBe(3);
+    expect(data[0].id).toBe(user1.id);
+    expect(data[1].id).toBe(user2.id);
+    expect(data[2].id).toBe(user3.id);
     expect(cursor.beforeCursor).toBeNull();
-    expect(typeof cursor.afterCursor === "string").toBeTruthy();
-    expect(data.length).toBe(2);
+    expect(cursor.afterCursor).toMatch(new RegExp("[a-zA-Z\\d]"));
   });
 });
