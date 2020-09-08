69227c3daad3ab973269e2dd2638a9882dc92209
Jason Raimondi <jason@raimondi.us>
2020-09-07 20:14:54 -0700

    fix: drop bull connection from users module
    all queues should be in queue.module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,9 +1,7 @@
-import { BullModule } from "@nestjs/bull";
 import { Module } from "@nestjs/common";
 
 import { MeResolver } from "~/app/user/resolvers/me.resolver";
 import { UserResolver } from "~/app/user/resolvers/user.resolver";
-import { QUEUE } from "~/lib/config/keys";
 import { EmailModule } from "~/lib/emails/email.module";
 import { RepositoryModule } from "~/lib/repositories/repository.module";
 
@@ -11,13 +9,6 @@ import { RepositoryModule } from "~/lib/repositories/repository.module";
   imports: [
     EmailModule,
     RepositoryModule,
-    BullModule.registerQueue({
-      name: QUEUE.email,
-      redis: {
-        host: "localhost",
-        port: 6379,
-      },
-    }),
   ],
   providers: [MeResolver, UserResolver],
 })
