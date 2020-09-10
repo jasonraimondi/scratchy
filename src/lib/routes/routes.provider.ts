b03fd57fceab1fb895bf30013a6acb1d5b5c2e01
Jason Raimondi <jason@raimondi.us>
2020-09-10 13:39:11 -0700

    refactor: use constant for di param names

--- src/lib/routes/routes.provider.ts
+++ src/lib/routes/routes.provider.ts
@@ -1,8 +1,9 @@
+import { SERVICES } from "~/config/keys";
 import { API_ROUTES } from "~/lib/routes/route.service";
 
 export const routesProviders = [
   {
-    provide: "routes",
+    provide: SERVICES.routes,
     useValue: API_ROUTES,
   },
 ];
