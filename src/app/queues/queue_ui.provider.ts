882c16379555fc49e53ecbb03486b951fe78f32a
Jason Raimondi <jason@raimondi.us>
2021-01-26 22:04:12 -0800

    chore: bump bull and bull-board

--- src/app/queues/queue_ui.provider.ts
+++ src/app/queues/queue_ui.provider.ts
@@ -1,13 +1,15 @@
 import { InjectQueue } from "@nestjs/bull";
 import { Injectable } from "@nestjs/common";
 import { Queue } from "bull";
-import { setQueues } from "bull-board";
+import { BullAdapter, setQueues } from "bull-board";
 
 import { QUEUE } from "~/config/queues";
 
 @Injectable()
 export class QueueUIProvider {
   constructor(@InjectQueue(QUEUE.email) private readonly emailQueue: Queue) {
-    setQueues([emailQueue]);
+    setQueues([
+      new BullAdapter(emailQueue),
+    ]);
   }
 }
