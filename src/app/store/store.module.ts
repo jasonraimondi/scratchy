67c51194eaf0ff7b290991422a8e8df741e169b6
Jason Raimondi <jason@raimondi.us>
2021-01-29 21:52:16 -0800

    drop: store jtest stuff

--- src/app/store/store.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ScalablePressModule } from "~/app/store/scalable_press/scalable_press.module";
-import { PaymentsModule } from "~/app/store/payments/payments.module";
-import { StorageModule } from "~/app/store/storage/storage.module";
-
-@Module({
-  imports: [StorageModule, ScalablePressModule, PaymentsModule],
-})
-export class StoreModule {}
