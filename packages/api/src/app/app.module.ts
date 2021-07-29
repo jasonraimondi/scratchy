484d0c2f819cf9297caa1119178632189ae3ee96
Jason Raimondi <jason@raimondi.us>
2021-07-28 21:35:05 -0700

    fix: set correct ports

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -10,20 +10,23 @@ import { QueueWorkerModule } from "~/lib/queue/queue_worker.module";
 import { LoggerModule } from "~/lib/logger/logger.module";
 import { JwtModule } from "~/lib/jwt/jwt.module";
 import { OAuthModule } from "~/app/oauth/oauth.module";
+import { QueueModule } from "~/lib/queue/queue.module";
 
 const imports = [];
 
-if (!ENV.isProduction && Date.now() < 0) imports.push(QueueWorkerModule);
+if (!ENV.isProduction) imports.push(QueueWorkerModule);
 
 @Module({
   imports: [
     ...imports,
-    AuthModule,
-    UserModule,
-    OAuthModule,
+    QueueModule,
     JwtModule,
     LoggerModule,
     GraphQLModule.forRoot(graphqlConfig),
+
+    AuthModule,
+    UserModule,
+    OAuthModule,
   ],
   controllers: [AppController],
 })
--- packages/api/src/config/queues.ts
+++ packages/api/src/config/queues.ts
@@ -8,7 +8,4 @@ export const QUEUE_JOBS: QueueJobs = {
   email: {
     send: "send",
   },
-  image: {
-    sync: "sync",
-  },
 };
