ea7b5b2a906eea98a6e7baf4057a55287c674a81
Jason Raimondi <jason@raimondi.us>
2022-03-30 22:04:54 -0700

    refactor: extract to validate_envs function

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,43 +1,22 @@
+
 process.env.TZ = "UTC";
 
-// import "reflect-metadata";
-// import "source-map-support/register";
 import "tsconfig-paths/register";
 import "dotenv/config";
 
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
-import { validate, ValidationError } from "class-validator";
 import { BullMqServer } from "bull-mq-transport";
 
 import { AppModule } from "~/app/app.module";
 import { ENV } from "~/config/environment";
 import { attachMiddlewares } from "~/lib/middleware/attach_middlewares";
-
-async function validateEnvironment() {
-  const validationErrors = await validate(ENV);
-  if (validationErrors.length === 0) return;
-  const errors = validationErrors
-    .reduce((prev, next) => {
-      const result = [...prev];
-      if (next.children?.length) result.push(...next.children);
-      return [next, ...result];
-    }, [] as ValidationError[])
-    .map((e) => {
-      if (!e.constraints) return undefined;
-      return Object.values(e.constraints).map((v) => {
-        const [first, ...split] = v.split(" ");
-        const result = [`"${first}"`, ...split].join(" ");
-        return `** ${e.target?.constructor.name} ${result} **`;
-      });
-    })
-    .filter((e) => e !== undefined);
-  throw new Error(`Invalid Environment \n\n${errors.join("\n- ")}\n`);
-}
+import { validateEnv } from "~/lib/utils/validate_envs";
 
 void (async () => {
-  await validateEnvironment();
+  await validateEnv();
+
   if (ENV.isDebug) console.log(ENV);
 
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
